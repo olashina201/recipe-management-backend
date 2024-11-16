483d4aae56d1fdb377074c261642b1a2447bc683
olashina201 <olashina201@gmail.com>
2024-11-16 20:32:03 +0100

    update edit recipe

--- src/controllers/recipe.controller.ts
+++ src/controllers/recipe.controller.ts
@@ -67,8 +67,8 @@ class RecipeController {
 
   updateRecipe = async (req: Request | any, res: Response) => {
     try {
-      const { recipeId } = req.params;
-      const result = await this.recipeService.updateRecipe(recipeId, req.body);
+      const { id } = req.params;
+      const result = await this.recipeService.updateRecipe(id, req.body);
       res.status(200).json(result);
     } catch (error: any) {
       res.status(error.status || 500).json({ message: error.message });
